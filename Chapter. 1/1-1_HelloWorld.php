391bb1a986cce4bd070cbc7ea93775bf52db5658
UmezakiDaisuke <60737352+UmezakiDaisuke@users.noreply.github.com>
2020-07-05 00:54:30 +0900

    1-1追加

---
+++ Chapter. 1/1-1_HelloWorld.php
@@ -0,0 +1,15 @@
+<head>
+<title>hello PHP</title>
+</head>
+
+<body>
+<?php
+
+    echo 'Hello PHP World!1'. "\n";
+    echo 'Hello PHP World!2'. "\n";
+    echo 'Hello PHP World!3'. "\n";
+    echo 'Hello PHP World!4'. "\n";
+    
+
+?>
+</body>
