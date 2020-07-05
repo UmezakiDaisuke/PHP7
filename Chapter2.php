f88f31f580b73549137971df88d19b2b533ecfb8
UmezakiDaisuke <60737352+UmezakiDaisuke@users.noreply.github.com>
2020-07-05 22:15:27 +0900

    'Chapter3追加'

--- Chapter2.php
+++ Chapter2.php
@@ -32,6 +32,50 @@ echo "<br>";
 print strtoupper('sato ichiro');
 echo "<br>";
 
+$string = "abcdefghijk";
+print substr($string, 2, 5);
+echo "<br>";
+
+print 1 * 8 . 3 * 4;
+echo "<br>";
+print (1 * 8) + 3 * 4;
+echo "<br>";
+
+// 2-9
+print 'Hello' == 'Hello'; //等価演算子「==」で値が一致（true）なら1を返却
+echo "<br>";
+print 'Hello' == 'HELLO'; //「==」で値が不一致（false）なら何も返却しない
+echo "<br>";
+print strcasecmp('Hello', 'Hello'); //strcasecmp: 大小文字を区別せず比較し、一致していれば0を返却
+echo "<br>";
+print strcasecmp('Hello', 'HELLO'); //値が一致しているので0返却
+echo "<br>";
+print strcasecmp('Hello', 'H'); //
+echo "<br>";
+
+// 2-10 ヒアドキュメント
+echo "Print string1 \"PHP\" <br>";
+
+echo <<<_DATA_
+Print string2 : "PHP" $string <br>
+_DATA_;
+
+echo "<br>";
+
+// 2-11
+$number = 0;
+
+$number += 1;
+$number += 2;
+$number++;
+$number--;
+++$number;
+--$number;
+
+
+print 'number : ' . "$number"; 
+echo "<br>";
+print 'number : ' . $number;
 
 ?>
 </body>
